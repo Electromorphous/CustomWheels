3c76b74f044be5dfd5b120ff582423d580cbebb2
Electromorphous <pstation.474@gmail.com>
2022-04-10 17:26:33 +0530

    remove not needed files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
-import * as THREE from 'three'
-import { Suspense, useLayoutEffect } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { useGLTF, MeshReflectorMaterial, Environment, Stage, PresentationControls } from '@react-three/drei'
+import * as THREE from "three";
+import { Suspense, useLayoutEffect } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  useGLTF,
+  MeshReflectorMaterial,
+  Environment,
+  Stage,
+  PresentationControls,
+} from "@react-three/drei";
 
 function Model(props) {
-  const { scene, nodes, materials } = useGLTF('/lambo.glb')
+  const { scene, nodes, materials } = useGLTF("/lambo.glb");
   useLayoutEffect(() => {
-    scene.traverse((obj) => obj.type === 'Mesh' && (obj.receiveShadow = obj.castShadow = true))
-    Object.assign(nodes.wheel003_020_2_Chrome_0.material, { metalness: 0.9, roughness: 0.4, color: new THREE.Color('#020202') })
-    Object.assign(materials.WhiteCar, { roughness: 0.0, metalness: 0.3, emissive: new THREE.Color('#500000'), envMapIntensity: 0.5 })
-  }, [scene, nodes, materials])
-  return <primitive object={scene} {...props} />
+    scene.traverse(
+      (obj) =>
+        obj.type === "Mesh" && (obj.receiveShadow = obj.castShadow = true)
+    );
+    Object.assign(nodes.wheel003_020_2_Chrome_0.material, {
+      metalness: 0.9,
+      roughness: 0.4,
+      color: new THREE.Color("#020202"),
+    });
+    Object.assign(materials.WhiteCar, {
+      roughness: 0.0,
+      metalness: 0.3,
+      emissive: new THREE.Color("#500000"),
+      envMapIntensity: 0.5,
+    });
+  }, [scene, nodes, materials]);
+  return <primitive object={scene} {...props} />;
 }
 
 export default function App() {
   return (
     <Canvas dpr={[1, 2]} shadows camera={{ fov: 45 }}>
-      <color attach="background" args={['#000']} />
-      <fog attach="fog" args={['#000', 10, 20]} />
+      <color attach="background" args={["#000"]} />
+      <fog attach="fog" args={["#000", 10, 20]} />
       <Suspense fallback={null}>
         <Environment path="/cube" />
-        <PresentationControls speed={1.5} global zoom={0.7} polar={[-0.1, Math.PI / 4]}>
-          <Stage environment={null} intensity={1} contactShadow={false} shadowBias={-0.0015}>
+        <PresentationControls
+          speed={1.5}
+          global
+          zoom={0.7}
+          polar={[-0.1, Math.PI / 4]}
+        >
+          <Stage
+            environment={null}
+            intensity={1}
+            contactShadow={false}
+            shadowBias={-0.0015}
+          >
             <Model scale={0.01} />
           </Stage>
           <mesh rotation={[-Math.PI / 2, 0, 0]}>
@@ -42,5 +70,5 @@ export default function App() {
         </PresentationControls>
       </Suspense>
     </Canvas>
-  )
+  );
 }
