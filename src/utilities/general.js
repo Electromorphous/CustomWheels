52f7164ba7f4b102291c766084ed7cb304a33f36
Electromorphous <pstation.474@gmail.com>
2022-05-08 15:50:54 +0530

    make utilities folder and file

---
+++ src/utilities/general.js
@@ -0,0 +1,20 @@
+export const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+export async function glow(part) {
+  for (let i = 0; i < 255; i += 8) {
+    await sleep(0);
+    part.current.material.emissive = {
+      r: i / 500,
+      g: i / 500,
+      b: i / 500,
+    };
+  }
+  for (let i = 255; i >= 0; i -= 8) {
+    await sleep(0);
+    part.current.material.emissive = {
+      r: i / 500,
+      g: i / 500,
+      b: i / 500,
+    };
+  }
+}
